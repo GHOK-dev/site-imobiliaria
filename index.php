1261fd8553ed03d0fc9508148c7f3b17550c23b8
CaueVaranda <caue.varanda@gmail.com>
2025-09-15 14:31:28 -0300

    Dyanmics
    Passa o mouse em cima do Imobifacil ai

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     <!-- Masthead-->
     <header class="masthead text-white text-center">
-        <div class="container d-flex align-items-center flex-column">
+        <div class="teto container d-flex align-items-center flex-column">
             <!-- Masthead Avatar Image-->
             <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
             <!-- Masthead Heading-->
